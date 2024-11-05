2fa971e72a84154948929ed2c08e0d654f024263
asirff399 <aintisar48@gmail.com>
2024-11-05 20:44:27 +0600

    [Add] - contact mail template

---
+++ app.js
@@ -0,0 +1,29 @@
+const contactUs = (event) => {
+    event.preventDefault(); 
+
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+
+    console.log(formData)
+    
+    // Directly use formData with fetch
+    fetch("http://127.0.0.1:8000/contact/", {
+        method: "POST",
+        body: formData, // Send the FormData directly
+    })
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok ' + res.statusText);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        // You can redirect or display a success message here
+        window.location.href = "./success.html";
+    })
+    .catch((error) => {
+        console.error("Error:", error); 
+        showError("There was a problem sending your message.");
+    });
+};
